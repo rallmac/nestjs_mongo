943b24e6aee0d2d918dbd3641169f6adddc80fd4
rallmac <tobijahekperikpe@gmail.com>
2025-05-28 08:28:22 +0100

    setting host in main.ts

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,10 @@ async function bootstrap() {
   const document = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
-
+  const port = process.env.PORT || 3000;
+  
+  // await app.listen(process.env.PORT ?? 3000);
+  // await app.listen(process.env.PORT || 3000);
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
